02cae2c3d05e21870b3ed43b3cd06ef8dffec18b
Chad Smith <smithchad17@gmail.com>
2024-12-17 08:39:56 -0500

    Updated structure

---
+++ Problems/4_FindMedianSortedArrays.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public static class _4_FindMedianSortedArrays
+    {
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int num1Length = nums1.Length;
+            int num2Length = nums2.Length;
+            double median = (num1Length + num2Length) / 2.0;
+            int ceiling = 0;
+
+            if ((num1Length + num2Length) % 2 == 0)
+            {
+                ceiling = (int)Math.Ceiling(median) + 1;
+            }
+            else
+            {
+                ceiling = (int)Math.Ceiling(median);
+            }
+
+            int[] mainArray = new int[ceiling];
+            int a = 0;
+            int b = 0;
+            int c = 0;
+
+            while (c < ceiling)
+            {
+                if (a < num1Length && b < num2Length)
+                {
+                    if (nums1[a] < nums2[b])
+                    {
+                        mainArray[c] = nums1[a];
+                        a++;
+                        c++;
+                    }
+                    else
+                    {
+                        mainArray[c] = nums2[b];
+                        c++;
+                        b++;
+                    }
+                }
+                else if (a < num1Length && b == num2Length)
+                {
+                    mainArray[c] = nums1[a];
+                    a++;
+                    c++;
+                }
+                else if (a == num1Length && b < num2Length)
+                {
+                    mainArray[c] = nums2[b];
+                    c++;
+                    b++;
+                }
+            }
+
+            if ((num1Length + num2Length) % 2 == 0)
+            {
+                return ((mainArray[ceiling - 1] + mainArray[ceiling - 2]) / 2.0);
+            }
+            else
+            {
+                return mainArray[ceiling - 1];
+            }
+
+        }
+
+
+    }
+}
---
+++ Problems/6_Convert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public static class _6_Convert
+    {
+        public static string Convert(string s, int numRows)
+        {
+            
+
+
+
+
+
+            return "";
+        }
+
+
+    }
+}
